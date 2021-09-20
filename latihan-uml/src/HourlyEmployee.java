f4760e68f0595a6780bca045fe501a06ddbe6ba5
muhaymi nurdin <82698265+muhaymi@users.noreply.github.com>
2021-09-20 13:36:14 +0700

    add class HourlyEmployee

--- latihan-uml/src/HourlyEmployee.java
+++ latihan-uml/src/HourlyEmployee.java
@@ -1,3 +1,26 @@
 class HourlyEmployee extends Employee{
     
+    private int hoursWorked;
+    private int hourlyPayment;
+    
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hw) {
+        hoursWorked = hw;
+    }
+
+    public int getHourlyPayment() {
+        return hourlyPayment;
+    }
+
+    public void setHourlyPayment(int hp) {
+        hourlyPayment = hp;
+    }
+
+    int payment(){
+        return getHourlyPayment() * getHoursWorked();
+    }
+    
 }
