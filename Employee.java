57f086bc64d5b8b3743b8ae4ca0d5e36b40f8606
muhaymi nurdin <82698265+muhaymi@users.noreply.github.com>
2021-09-20 08:54:41 +0700

    add HourlyEmploye

--- Employee.java
+++ Employee.java
@@ -20,3 +20,40 @@ abstract class Employee {
 
     abstract int payment();
 }
+
+
+
+
+
+
+
+
+
+
+//add HourlyEmploye
+
+class HourlyEmployee extends Employee{
+    private int hoursWorked;
+    private int hourlyPayment;
+    
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hw) {
+        hoursWorked = hw;
+    }
+
+    public int getHourlyPayment() {
+        return hourlyPayment;
+    }
+
+    public void setHourlyPayment(int hp) {
+        hourlyPayment = hp;
+    }
+
+    int payment(){
+        return getHourlyPayment() * getHoursWorked();
+    }
+    
+}
